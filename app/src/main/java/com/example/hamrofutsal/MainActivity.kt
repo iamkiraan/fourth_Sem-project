dc9a89e4dca37e30e2539a6905893885e624378f
beta_astra <kiran.211720@gmail.com>
2024-01-29 18:06:54 +0545

    ui for booking futsals

--- app/src/main/java/com/example/hamrofutsal/MainActivity.kt
+++ app/src/main/java/com/example/hamrofutsal/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.hamrofutsal
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
@@ -10,6 +9,7 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     //private lateinit var message: TextView
@@ -31,7 +31,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val decorView: View = window.decorView
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        decorView.systemUiVisibility =View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
